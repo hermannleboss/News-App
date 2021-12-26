ab877517d5abccdac2be77ffae9b050fd98052ec
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-26 23:36:51 +0000

    Init of controllers

---
+++ back/controllers/NewsController.js
@@ -0,0 +1,6 @@
+const News=require('../models/News');
+exports.createNews=(req, res, next) => {}
+exports.updateNews=(req, res, next) => {}
+exports.deleteNews=(req, res, next) => {}
+exports.getOneNews=(req, res, next) => {}
+exports.getAllNews=(req, res, next) => {}
---
+++ back/controllers/UserController.js
@@ -0,0 +1,3 @@
+const User=require('../models/User');
+exports.createUser=(req, res, next) => {}
+exports.updateUser=(req, res, next) => {}

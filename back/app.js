8dbdf2d434441fd8944ad3798ebf95e6fb1c4ef6
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-26 23:12:51 +0000

    Connect to Mongobd OK

--- back/app.js
+++ back/app.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const mongoose=require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb+srv://admin:admin@cluster0.wvu2u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');

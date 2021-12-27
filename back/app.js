95fc98d8427774ac195ab64d58ce4559c7d440f6
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-27 13:29:50 +0000

    Create News, get all news OK

--- back/app.js
+++ back/app.js
@@ -11,6 +11,8 @@ mongoose.connect('mongodb+srv://admin:admin@cluster0.wvu2u.mongodb.net/myFirstDa
     })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
--- back/controllers/NewsController.js
+++ back/controllers/NewsController.js
@@ -1,6 +1,18 @@
 const News=require('../models/News');
-exports.createNews=(req, res, next) => {}
+exports.createNews=(req, res, next) => {
+    delete req.body._id;
+    const news = new News({
+        ...req.body
+    });
+    news.save()
+        .then(() => res.status(201).json({message: 'Object saved'}))
+        .catch(error => res.status(400).json({error}));
+}
 exports.updateNews=(req, res, next) => {}
 exports.deleteNews=(req, res, next) => {}
 exports.getOneNews=(req, res, next) => {}
-exports.getAllNews=(req, res, next) => {}
+exports.getAllNews=(req, res, next) => {
+    News.find()
+        .then(news => res.status(200).json(news))
+        .catch(error => res.status(400).json({error}));
+}

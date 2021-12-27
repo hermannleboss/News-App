37b15975d43f397181621d94fc0c115c5c5d0f03
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-27 13:49:26 +0000

    Manage of News CRUD in controller

--- back/controllers/NewsController.js
+++ back/controllers/NewsController.js
@@ -1,5 +1,5 @@
-const News=require('../models/News');
-exports.createNews=(req, res, next) => {
+const News = require('../models/News');
+exports.createNews = (req, res, next) => {
     delete req.body._id;
     const news = new News({
         ...req.body
@@ -8,10 +8,40 @@ exports.createNews=(req, res, next) => {
         .then(() => res.status(201).json({message: 'Object saved'}))
         .catch(error => res.status(400).json({error}));
 }
-exports.updateNews=(req, res, next) => {}
-exports.deleteNews=(req, res, next) => {}
-exports.getOneNews=(req, res, next) => {}
-exports.getAllNews=(req, res, next) => {
+exports.updateNews = (req, res, next) => {
+    News.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+        .then(() => res.status(201).json({message: 'Object Updated'}))
+        .catch(error => res.status(400).json({error}));
+}
+exports.deleteNews = (req, res, next) => {
+    console.log("delete");
+    News.findOne({_id: req.params.id})
+        .then((news) => {
+            if (!news) {
+                return res.status(404).json({
+                    error: new Error('Object not found')
+                });
+            }
+            /*
+            Check of auth of user
+            if (news.userId != req.auth.userId) {
+                return res.status(401).json({
+                    error: new Error('Request not authorized')
+                });
+            }*/
+            News.deleteOne({_id: req.params.id})
+                .then(() => res.status(200).json({message: 'Object Deleted'}))
+                .catch(error => res.status(400).json({error}));
+        })
+        .catch(error => res.status(400).json({error}));
+}
+exports.getOneNews = (req, res, next) => {
+    console.log("get ONe news");
+    News.findOne({_id: req.params.id})
+        .then(news => res.status(200).json(news))
+        .catch(error => res.status(400).json({error}));
+}
+exports.getAllNews = (req, res, next) => {
     News.find()
         .then(news => res.status(200).json(news))
         .catch(error => res.status(400).json({error}));

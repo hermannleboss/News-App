665e762f84d8c17cdbd5cced4f1d105ea402393b
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-27 18:03:21 +0000

    Add of auth on router

--- back/route/NewsRoute.js
+++ back/route/NewsRoute.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router=express.Router();
 const newsController=require('../controllers/NewsController');
+const auth=require('../middleware/auth');
 
-router.post('/' , newsController.createNews);
-router.put('/:id' , newsController.updateNews);
-router.delete('/:id' , newsController.deleteNews);
+router.post('/' , auth, newsController.createNews);
+router.put('/:id' ,  auth,newsController.updateNews);
+router.delete('/:id' , auth, newsController.deleteNews);
 router.get('/:id' , newsController.getOneNews);
 router.get('/' , newsController.getAllNews);
 

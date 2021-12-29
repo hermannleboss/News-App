3ec261a18738c26cb6def7c2029251f1cca181db
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-29 20:31:29 +0000

    Apply of style

--- front/src/components/ListNews.js
+++ front/src/components/ListNews.js
@@ -1,9 +1,12 @@
 import NewsCard from "./NewsCard";
+import logo from "../assets/logo.svg";
 function ListNews(){
     return(
         <div>
             <h2>List Of News</h2>
-            <div>
+            <div className="flex flex-wrap -m-4 mt-3">
+                <NewsCard></NewsCard>
+                <NewsCard></NewsCard>
                 <NewsCard></NewsCard>
                 <NewsCard></NewsCard>
             </div>
--- front/src/components/NewsCard.js
+++ front/src/components/NewsCard.js
@@ -2,12 +2,14 @@ import logo from "../assets/logo.svg";
 import "../styles/NewsCard.scss"
 function CreateNews() {
     return (
-        <div className="news-card">
-            <img src={logo} alt="Avatar"/>
-                <div className="container">
-                    <h4><b>Article Title</b></h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus amet corporis, cumque doloribus ea earum error, excepturi exercitationem harum nemo nostrum obcaecati officia perspiciatis quisquam, quod soluta temporibus veritatis voluptatum.</p>
-                </div>
+        <div className="xl:w-1/4 md:w-1/2 p-4 ">
+            <div className="p-6 rounded-lg border-2 border-white rounded-lg">
+                <img className="h-40 rounded w-full object-full object-center mb-6"
+                     src={logo} alt="content"/>
+                <h2 className="text-lg font-medium title-font mb-4">Chichen Itza</h2>
+                <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit
+                    waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+            </div>
         </div>
     )
 }

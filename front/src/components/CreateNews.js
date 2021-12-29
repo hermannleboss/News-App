72d218d9bc107f4a034bb23736ce84e74c769ff9
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-29 20:22:01 +0000

    Update form style

--- front/src/components/CreateNews.js
+++ front/src/components/CreateNews.js
@@ -4,10 +4,24 @@ function CreateNews() {
             <h2>Create News</h2>
 
             <form action="">
-                <div><label htmlFor="">title</label><input type="text"/></div>
-                <div><label htmlFor="">description</label><input type="text"/></div>
-                <div><label htmlFor="">imageUrl</label><input type="text"/></div>
-                <button>Create News</button>
+                <div className="relative mb-4">
+                    <label htmlFor="title" className="leading-7 text-gray-300">Title</label>
+                    <input type="text" id="title" name="title"
+                           className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                </div>
+                <div className="relative mb-4">
+                    <label htmlFor="description" className="leading-7 text-gray-300">Description</label>
+                    <textarea id="description" name="description"
+                              className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                </div>
+                <div className="relative mb-4">
+                    <label htmlFor="imageUrl" className="leading-7 text-gray-300">ImageUrl</label>
+                    <input type="imageUrl" id="imageUrl" name="imageUrl"
+                           className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                </div>
+                <button
+                    className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Create News
+                </button>
             </form>
         </div>
     )

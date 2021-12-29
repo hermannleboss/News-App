7dd57c551fddb2b932bf423e056186f34cdd10ef
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-29 19:24:42 +0000

    Create form OK

--- front/src/components/App.js
+++ front/src/components/App.js
@@ -8,7 +8,7 @@ function App() {
     return (
         <div className="App">
             <Nav></Nav>
-            <div>
+            <div className="App-content">
                 <CreateNews></CreateNews>
                 <Action></Action>
                 <ListNews></ListNews>
--- front/src/components/CreateNews.js
+++ front/src/components/CreateNews.js
@@ -1,8 +1,16 @@
-function CreateNews(){
-    return(
+function CreateNews() {
+    return (
         <div>
             <h2>Create News</h2>
+
+            <form action="">
+                <div><label htmlFor="">title</label><input type="text"/></div>
+                <div><label htmlFor="">description</label><input type="text"/></div>
+                <div><label htmlFor="">imageUrl</label><input type="text"/></div>
+                <button>Create News</button>
+            </form>
         </div>
     )
 }
+
 export default CreateNews

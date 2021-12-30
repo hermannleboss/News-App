b8362a6d43ef7771cc612a40dfe294dce90e2387
EHO K. Hermann <hermannleboss@gmail.com>
2021-12-30 19:20:15 +0000

    Map of News Card with datas

--- front/src/components/ListNews.js
+++ front/src/components/ListNews.js
@@ -1,14 +1,16 @@
 import NewsCard from "./NewsCard";
-import logo from "../assets/logo.svg";
+import {newsList} from '../datas/NewsList';
+
 function ListNews(){
     return(
         <div>
             <h2>List Of News</h2>
             <div className="flex flex-wrap -m-4 mt-3">
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
-                <NewsCard></NewsCard>
+                {
+                    newsList.map((news)  => (
+                        <NewsCard key={news.title}></NewsCard>
+                    ))
+                }
             </div>
         </div>
     )
